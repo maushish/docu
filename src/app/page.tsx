a676d12a7f0cd8522c04353181ca15d73c4a162d
maushish <chamarnumber102@gmail.com>
2023-10-17 04:58:50 +0530

    Added some basic styles in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
+import {ArrowRight} from "lucide-react"
 
 export default function Home() {
   return (
@@ -8,6 +10,13 @@ export default function Home() {
           Docu is now public 
         </p>
       </div>
+      <h1 className=" max-w-4xl text-5xl font-bold  md:text-6xl lg:text-7xl">Chat with your <span className=" text-blue-600">documents</span></h1>
+      <p className="mt-5 max-w-prose text-z sm:text-lg">
+        Docu allows you to have conversations with any PDF document.Simply upload your file and start asking questions right away.
+      </p>
+
+
+      <Link href='/dashboard' target="_blank">Get started <ArrowRight className="ml-2 h-5 w-5 "/> </Link>
     </MaxWidthWrapper>
 
   )
