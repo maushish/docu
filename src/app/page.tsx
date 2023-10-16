df40be42920a823e39a585b78cdeb6051e3ea430
maushish <chamarnumber102@gmail.com>
2023-10-17 05:12:41 +0530

    Added shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import {ArrowRight} from "lucide-react"
-
 export default function Home() {
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import clsx, { ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-export function cn(...inputs: ClassValue[]){
-    return twMerge(clsx(inputs)) //px-2 py-2 in tailwind merge u can write these in p-2 and clsx helps to resolve conflicts
-    //basically you can use same qualities multiple times
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
